cde1eabef3fc4c1a342b6cbb63ffc4ff31551c51
Asish <iamasish3001@gmail.com>
2020-01-10 18:15:25 +0530

    restored the code

--- new_learning/music/views.py
+++ new_learning/music/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("<h1> QUANTUM COMPUTING IS THE FUTURE")
+    return HttpResponse("<p> Ernst and Yound</p>")
+
