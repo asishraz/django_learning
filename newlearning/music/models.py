138bd4f466f55bfb89f9060c7df43143732f006c
Asish <iamasish3001@gmail.com>
2020-01-13 13:28:48 +0530

    learned about migartions in django

--- newlearning/music/models.py
+++ newlearning/music/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    artist = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    album_title = models.CharField(max_length=200)
+    album_logo = models.CharField(max_length=1200)
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    song_title = models.CharField(max_length=200)
+    song_type = models.CharField(max_length=10)
+
